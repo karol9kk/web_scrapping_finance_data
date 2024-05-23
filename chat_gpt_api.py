2a74d05035a457d16317bfb5415a4be3e1690854
karol9kk <karol9kk@gmail.com>
2024-05-23 14:35:13 +0200

    chat gpt-api implementation

---
+++ chat_gpt_api.py
@@ -0,0 +1,41 @@
+from openai import OpenAI
+from config import OPENAI_API_KEY
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def get_stock_symbol(input):
+
+    input="Google"
+
+    messages=[
+        {
+        "role": "system",
+        "content": [
+            {
+            "type": "text",
+            "text": "Give only a stock symbol"
+            }
+        ]
+        },
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": f"{input}"
+            }
+        ]
+        }
+    ]
+
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=messages,
+    temperature=1,
+    max_tokens=10,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+
+
+    symbol=response.choices[0].message.content
