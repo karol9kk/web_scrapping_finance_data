ee2b31aab2a8c51d77d0b6d20598e6116b583766
karol9kk <karol9kk@gmail.com>
2024-05-23 12:04:32 +0200

    fixed score for negative labels

--- nlp_function.py
+++ nlp_function.py
@@ -6,13 +6,15 @@ from transformers import pipeline
 
 
 def sentiment_with_date_and_org(url):
+
+    model='mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis'
     
     #extraction of data
     text=extract_text_from_div(url)
     date=extract_date_from_url(url)
     #sumarize
     summarizer=pipeline("summarization")
-    outputs=summarizer(text,max_length=57,clean_up_tokenization_spaces=True)
+    outputs=summarizer(text,max_length=100,clean_up_tokenization_spaces=True)
     summarized_text_str = str(outputs[0]['summary_text'])
     
     #entity detection
@@ -21,8 +23,12 @@ def sentiment_with_date_and_org(url):
     org_entities = [entity['word'] for entity in outputs_ner if entity['entity_group']== 'ORG']
     org_entities=org_entities[0]
     #sentiment 
-    classifier=pipeline("text-classification")
+    classifier=pipeline("text-classification",model=model)
+    label=classifier(summarized_text_str)[0]['label']
     outputs_sentiment=classifier(summarized_text_str)[0]['score']
+    
+    if label=="negative": outputs_sentiment*(-1)
+    
   
 
     return date,org_entities,outputs_sentiment
