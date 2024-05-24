7f9f19687de07028bc13c090d3d964ae3980b679
karol9kk <karol9kk@gmail.com>
2024-05-24 17:58:22 +0200

    added entity detection

--- chat_gpt_api.py
+++ chat_gpt_api.py
@@ -5,7 +5,6 @@ client = OpenAI(api_key=OPENAI_API_KEY)
 def get_stock_symbol(input):
 
     
-
     messages=[
         {
         "role": "system",
@@ -37,7 +36,41 @@ def get_stock_symbol(input):
     presence_penalty=0
     )
 
-
     symbol=response.choices[0].message.content
 
     return symbol
+
+
+def get_company(input):
+
+    
+    messages=[
+        {
+        "role": "system",
+        "content": [
+            {
+            "type": "text",
+            "text": "You are a financial reporter/ Give me only name of company, about which article is written about "
+            }
+        ]
+        },
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": f"{input}"
+            }
+        ]
+        }
+    ]
+
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=messages,
+    temperature=1,
+    max_tokens=10,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
